7aba3cd756b084d4ca2bbd6441479b14ad5d0833
Tim Doren <dorentim@gmail.com>
2023-02-14 11:43:03 -0500

    Add project files.

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,18 @@
+﻿for (int i = 1; i <= 30; i++)
+{
+    if (i % 3 == 0 && i % 5 != 0)
+    {
+        Console.Write($"{i} ");
+        Console.WriteLine("Fizz");
+    }
+    if (i % 5 == 0 && i % 3 != 0)
+        {
+        Console.Write($"{i} ");
+        Console.WriteLine("Buzz");
+        }
+    if (i % 3 == 0 && i % 5 == 0)
+    {
+        Console.Write($"{i} ");
+        Console.WriteLine("FizzBuzz");
+    }
+}
